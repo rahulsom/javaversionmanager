969acdbd9c2dead2665cdd7294216dc1e2aa0ed0
Rahul Somasunderam <rahul.som@gmail.com>
2016-09-27 18:00:08 -0700

    Add security to cron

--- glide.groovy
+++ glide.groovy
@@ -1,10 +1,17 @@
 app {
-    name = "javaversionmanager"
-    version = "1"
+  name = "javaversionmanager"
+  version = "1"
 }
 
 cron {
-    entries = [
-            [url: "/admin/reload", description: "reload versions", schedule: "every day 00:00"],
-    ]
+  entries = [
+      [url: "/admin/reload", description: "reload versions", schedule: "every day 00:00"],
+  ]
+}
+
+web {
+  security = [
+      '*'    : ['/versions', '/'],
+      'admin': ["/admin/*"],
+  ]
 }
