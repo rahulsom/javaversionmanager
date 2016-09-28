251414ee161c4c24708f369e1681995d76edbb02
Rahul Somasunderam <rahul.som@gmail.com>
2016-09-27 18:56:37 -0700

    Fix version url

--- app/_routes.groovy
+++ app/_routes.groovy
@@ -1,4 +1,4 @@
 get '/',              forward: '/index.html'
 get '/builds',        forward: '/builds.groovy'
-get '/versions',      forward: '/versionNumbers.groovy'
+get '/versions',      forward: '/versions.groovy'
 get '/admin/reload',  forward: '/admin/reload.groovy'
