39e890fa70969480f62dd17f18735f6001c03eef
Rahul Somasunderam <rahul.som@gmail.com>
2016-10-01 18:44:52 -0700

    Clean up routes

--- app/_routes.groovy
+++ app/_routes.groovy
@@ -1,5 +1,12 @@
-get '/',              forward: '/index.html'
-get '/builds',        forward: '/builds.groovy'
-get '/versions',      forward: '/versions.groovy'
-get '/tags',          forward: '/tags.groovy'
-get '/admin/reload',  forward: '/admin/reload.groovy'
+get '/',                        forward: '/index.html'
+
+get '/builds',                  forward: '/builds.groovy'
+get '/builds/t/@tags',          forward: '/builds.groovy?tags=@tags'
+get '/builds/t/@tags/@version', forward: '/builds.groovy?tags=@tags&version=@version'
+get '/builds/v/@version',       forward: '/builds.groovy?version=@version'
+
+get '/versions',                forward: '/versions.groovy'
+
+get '/tags',                    forward: '/tags.groovy'
+
+get '/admin/reload',            forward: '/admin/reload.groovy'
