dfde523631576a276e31920f6a1cb0bfb2588781
Rahul Somasunderam <rahul.som@gmail.com>
2016-10-05 09:39:42 -0700

    Retry on reload job

--- app/admin/reload.groovy
+++ app/admin/reload.groovy
@@ -2,6 +2,18 @@ package admin
 
 import com.github.rahulsom.jvm.JavaServiceFacade
 
-new JavaServiceFacade().getBuilds(true)
+def retries = 3
+Exception lastException = null
+if (retries) {
+    try {
+        new JavaServiceFacade().getBuilds(true)
+    } catch (Exception e) {
+        lastException = e
+        sleep 10000
+        retries --
+    }
+} else {
+    throw lastException
+}
 
 println "Done"
